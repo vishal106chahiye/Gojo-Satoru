56c007a085f3b09ab27e5cb1cf673a7bd0690f11
vishal106chahiye <118664355+vishal106chahiye@users.noreply.github.com>
2022-11-20 16:47:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['918178835721']
+global.premium = ['918178835721']
+global.ownernomer = '918178835721'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
